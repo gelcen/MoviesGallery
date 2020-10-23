b26d133a0626a37080e7e50c251b01e378fc89fd
Gelcen Savynday <v-geltsens@k2it.ru>
2020-10-23 21:23:46 +0700

    Add movie model and database context

---
+++ MoviesGallery.App/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace MoviesGallery.App.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+
+    }
+}
