49728e989bb5c070a650985c54555894a25d6181
Gelcen Savynday <v-geltsens@k2it.ru>
2020-10-23 21:42:23 +0700

    Add views, bootstrap

---
+++ MoviesGallery.App/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesGallery.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
